f2c020877efcbf1b4e3f4365171c761ef2c9009a
Gus Workman <gus.a.workman@gmail.com>
2017-03-04 19:51:24 -0500

    Added TV options

--- app/src/main/java/jacob/ryan/gus/com/darwin/homeautomation/MainActivity.java
+++ app/src/main/java/jacob/ryan/gus/com/darwin/homeautomation/MainActivity.java
@@ -1,6 +1,7 @@
 package jacob.ryan.gus.com.darwin.homeautomation;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     Button lightsButton, tvButton, musicButton;
     TextView temperatureData, humidityData, powerData, entrancesData;
     ImageView lightsImage, tvImage, musicImage;
+    View lightOptions, tvOptions;
+
+    ImageView pickerRed, pickerOrange, pickerYellow, pickerGreen, pickerDarkGreen, pickerBlue, pickerDarkBlue, pickerPurple;
+    RelativeLayout pickerWhite;
 
     boolean lightsOn = false, tvOn = false, musicOn = false;
 
@@ -44,6 +50,19 @@ public class MainActivity extends AppCompatActivity {
         lightsImage = (ImageView) findViewById(R.id.icon_lights);
         tvImage = (ImageView) findViewById(R.id.icon_tv);
         musicImage = (ImageView) findViewById(R.id.icon_music);
+        lightOptions = findViewById(R.id.main_color_options);
+        tvOptions = findViewById(R.id.main_tv_options);
+
+        // these are the color picker views
+        pickerRed = (ImageView) findViewById(R.id.color_picker_red);
+        pickerOrange = (ImageView) findViewById(R.id.color_picker_orange);
+        pickerYellow = (ImageView) findViewById(R.id.color_picker_yellow);
+        pickerGreen = (ImageView) findViewById(R.id.color_picker_green);
+        pickerDarkGreen = (ImageView) findViewById(R.id.color_picker_dark_green);
+        pickerBlue = (ImageView) findViewById(R.id.color_picker_blue);
+        pickerDarkBlue = (ImageView) findViewById(R.id.color_picker_dark_blue);
+        pickerPurple = (ImageView) findViewById(R.id.color_picker_purple);
+        pickerWhite = (RelativeLayout) findViewById(R.id.color_picker_white);
     }
 
     public void setup_listeners() {
@@ -51,10 +70,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!lightsOn) {
-                    lightsImage.setImageResource(R.drawable.ic_power_white);
+                    if (tvOptions.getVisibility() == View.VISIBLE) {
+                        tvOptions.setVisibility(View.GONE);
+                    }
+                    lightOptions.setVisibility(View.VISIBLE);
                     lightsOn = true;
                 } else {
-                    lightsImage.setImageResource(R.drawable.ic_power_grey);
+                    lightOptions.setVisibility(View.GONE);
                     lightsOn = false;
                 }
             }
@@ -64,10 +86,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!tvOn) {
-                    tvImage.setImageResource(R.drawable.ic_power_white);
+                    if (lightOptions.getVisibility() == View.VISIBLE) {
+                        lightOptions.setVisibility(View.GONE);
+                    }
+                    tvOptions.setVisibility(View.VISIBLE);
                     tvOn = true;
                 } else {
-                    tvImage.setImageResource(R.drawable.ic_power_grey);
+                    tvOptions.setVisibility(View.GONE);
                     tvOn = false;
                 }
             }
@@ -86,5 +111,89 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        pickerRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerRed.setImageResource(R.drawable.color_red);
+            }
+        });
+
+        pickerOrange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerOrange.setImageResource(R.drawable.color_orange);
+            }
+        });
+
+        pickerYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerYellow.setImageResource(R.drawable.color_yellow);
+            }
+        });
+
+        pickerGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerGreen.setImageResource(R.drawable.color_green);
+            }
+        });
+
+        pickerDarkGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerDarkGreen.setImageResource(R.drawable.color_dark_green);
+            }
+        });
+
+        pickerBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerBlue.setImageResource(R.drawable.color_blue);
+            }
+        });
+
+        pickerDarkBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerDarkBlue.setImageResource(R.drawable.color_dark_blue);
+            }
+        });
+
+        pickerPurple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerPurple.setImageResource(R.drawable.color_purple);
+            }
+        });
+
+        pickerWhite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset_color_picker();
+                pickerWhite.setBackgroundResource(R.drawable.color_white);
+            }
+        });
+
+    }
+
+    public void reset_color_picker() {
+        pickerRed.setImageResource(R.color.picker_red);
+        pickerOrange.setImageResource(R.color.picker_orange);
+        pickerYellow.setImageResource(R.color.picker_yellow);
+        pickerGreen.setImageResource(R.color.picker_green);
+        pickerDarkGreen.setImageResource(R.color.picker_dark_green);
+        pickerBlue.setImageResource(R.color.picker_blue);
+        pickerDarkBlue.setImageResource(R.color.picker_dark_blue);
+        pickerPurple.setImageResource(R.color.picker_purple);
+        pickerWhite.setBackgroundResource(R.color.white);
     }
 }
